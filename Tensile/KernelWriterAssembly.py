01fd0cc46b11259e31a002571955b02698442e9b
Ben Sander <ben.sander@amd.com>
2019-01-13 07:20:01 -0500

    Use s_cbranch_scc0 for unroll loop restart

--- Tensile/KernelWriterAssembly.py
+++ Tensile/KernelWriterAssembly.py
@@ -4022,13 +4022,16 @@ class KernelWriterAssembly(KernelWriter):
         sgpr("LoopCounters+%u"%loopIdx), \
         hex(endCounter), \
         "counter%s==0"%(loopChar) )
-    kStr += inst("s_cbranch_scc1 label_%04u"%(loopLabelEnd if finalLoop else loopLabelEndOddExit), \
-        "exit Loop%s"%loopChar )
-
-    if finalLoop:
-      kStr += inst("s_branch label_%04u"%loopLabelBegin, \
-          "restart %s Loop%s"%("tailLoop" if tailLoop else "unrolled loop", loopChar ))
-      kStr += "label_%04u: // unroll loop odditer exit\n" % (loopLabelEndOddExit)
+    if not finalLoop:
+      kStr += inst("s_cbranch_scc1 label_%04u"%(loopLabelEndOddExit), "exit Loop%s"%loopChar )
+    else: #finalLoop:
+      if not tailLoop:
+        kStr += inst("s_cbranch_scc0 label_%04u"%loopLabelBegin, \
+            "restart %s Loop%s"%("unrolled loop", loopChar ))
+        kStr += "label_%04u: // unroll loop odditer exit\n" % (loopLabelEndOddExit)
+      else: # tailLoop:
+        kStr += inst("s_branch label_%04u"%loopLabelBegin, \
+            "restart %s Loop%s"%("tailLoop" if tailLoop else "unrolled loop", loopChar ))
       if not kernel["SuppresssNoLoadLoop"] and kernel["ExpandPointerSwap"]:
         # In this case we kept the 'no-load' loop which has LDS offsets assuming first bank of LDS
         # if we exit the main loop at an odd iter - need to swap LDS read pointers
