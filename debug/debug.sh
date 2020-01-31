eedb566df489ffdddd0b1165081f06d00f9f2700
timlathy <tim.labushev@gmail.com>
2020-01-31 11:01:24 -0800

    remove hard dependency on gfx906

--- debug/debug.sh
+++ debug/debug.sh
@@ -36,8 +36,8 @@ load-file = "tmp/debug.co"
 exec-before-load = """bash -o pipefail -c '\
   perl breakpoint.pl -ba \$ASM_DBG_BUF_ADDR -bs \$ASM_DBG_BUF_SIZE \
     -l $line -w "$watches" -s 96 -r s0 -t ${counter:=0} -p $perl_args $source_file \
-  | /opt/rocm/opencl/bin/x86_64/clang -x assembler -target amdgcn--amdhsa -mcpu=gfx906 -mno-code-object-v3 \
-    -Igfx9/include -o tmp/debug.co -'"""
+  | /opt/rocm/opencl/bin/x86_64/clang -x assembler -target amdgcn--amdhsa -mcpu=`/opt/rocm/bin/rocminfo | grep -om1 gfx9..` \
+    -mno-code-object-v3 -Igfx9/include -o tmp/debug.co -'"""
 EOF
 
 export HSA_TOOLS_LIB=libplugintercept.so
